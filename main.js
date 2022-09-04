9bdd4e70bdc780957cfd658be218d7e7095eb0af
hyunchanwi <105478212+hyunchanwi@users.noreply.github.com>
2022-09-04 19:57:00 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    const hw = document.getElementById('hw');
+    hw.addEventListener('click', function () {
+        alert('버튼 되게 신기하시죠? 이거하는데 1시간 동안 했..');
+    })
+}
