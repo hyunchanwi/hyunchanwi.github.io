a55de86993a33234b37cbc6245aacb3b6369bdfd
hyunchanwi <hyunchan09@gmail.com>
2022-09-05 15:03:52 +0900

    9/5 3:03

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 window.onload = function () {
     const hw = document.getElementById('hw');
     hw.addEventListener('click', function () {
-        alert('버튼 되게 신기하시죠? 이거하는데 1시간 동안 했..');
+        alert('버튼 되게 신기하지? 이거하는데 1시간 동안 했어..');
+    })
+}
+window.onload = function () {
+    const i2 = document.getElementById('i2');
+    i2.addEventListener('click', function () {
+        alert('버튼 이쁨 ㅎ');
     })
 }
